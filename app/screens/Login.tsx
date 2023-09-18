28272a1b75c47840709970e03666a12ad1093506
LaraVic9 <laravicspereira@gmail.com>
2023-09-18 17:44:22 -0300

    implementando layout

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import { View, Text, StyleSheet, TextInput, ActivityIndicator, Button, KeyboardAvoidingView } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, ActivityIndicator, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
 import { FIREBASE_AUTH } from '../../config/firebaseConfig'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { Button } from 'native-base'
 
 const Login = () => {
     const [email, setEmail] = useState('')
@@ -44,8 +47,28 @@ const Login = () => {
                 <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder='Password' autoCapitalize='none' onChangeText={(text) => setPassword(text)}></TextInput>
                 { loading ? <ActivityIndicator size="large" color="#0000ff" /> : 
                 <>
-                    <Button title="Login" onPress={signIn}/>
-                    <Button title="Create Account" onPress={signUp}/>
+                    <TouchableOpacity style={styles.containerButton} >
+                        <LinearGradient
+                            // Button Linear Gradient
+                            colors={['#FFB677', '#FF3CBD']}
+                            start={{x: 0, y: 0}}
+                            end={{x:1, y: 1}}
+                            style={styles.buttonLogin}
+                        >
+                            <Text style={styles.textButton} onPress={signUp}>LOGIN</Text>
+                        </LinearGradient>
+                        <LinearGradient
+                            // Button Linear Gradient
+                            colors={['#FFB677', '#FF3CBD']}
+                            start={{x: 0, y: 0}}
+                            end={{x:1, y: 1}}
+                            style={styles.buttonSignUp}
+                        >
+                            <Text style={styles.textButton} onPress={signIn}>CREATE ACCOUNT</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                   
+                    
                 </>}
         </KeyboardAvoidingView>
         </View>
@@ -67,5 +90,66 @@ const styles = StyleSheet.create({
         borderRadius: 25, 
         padding: 10,
         backgroundColor: '#fff'
-    }
-})
+    },
+        mainContainer: {
+          flex: 1,
+          backgroundColor: '#f1f1f1',
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+        titulo: {
+          fontSize: 60,
+          color: '#34434D',
+          fontWeight: 'bold'
+        },
+        subTitle: {
+          fontSize: 20,
+          color: 'gray',
+        },
+        textInput: {
+          borderWidth: 1,
+          paddingStart: 30,
+          borderColor: 'gray',
+          padding: 10,
+          width: '80%',
+          height: 50,
+          marginTop: 20,
+          borderRadius: 30,
+          backgroundColor: '#fff'
+        },
+        fotgotPassword: {
+          fontSize: 14,
+          color: 'gray',
+          marginTop: 20
+        },
+        containerButton: {
+           
+            flex: 1,
+            alignItems: 'center',
+      },
+      textButton: {
+          fontSize: 14,
+          color: '#fff',
+          fontWeight: 'bold',
+        },
+      buttonLogin: {
+         width: '90%',
+         height: 50,
+         borderRadius: 25,
+         padding: 10,
+         alignItems: 'center',
+         justifyContent: 'center',
+         marginTop: 40
+      },
+      buttonSignUp: {
+        width: '90%',
+        height: 50,
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+      }
+        
+      });
+      
